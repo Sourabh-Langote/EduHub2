0defd17605324b555ae460927cfda9d0f7dd530c
sourabhlangote <sourabh.langote@mobicule.com>
2023-10-16 10:42:25 +0530

    Registration page added

---
+++ app/src/debug/kotlin/loginModule/registrationModule/RegistrationPage.kt
@@ -0,0 +1,12 @@
+package loginModule.registrationModule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.eduhub.R
+
+class RegistrationPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration_page)
+    }
+}
